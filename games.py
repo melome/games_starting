d1dc4938128c5492b740d4560e63028f321237ac
melome <shanleyrosero@gmail.com>
2020-05-19 19:54:20 +0800

    additional function for highest card and roulette game

--- games.py
+++ games.py
@@ -29,8 +29,32 @@ def cho_han(guess, bet):
         return "Better luck next time.. the dice is " + dice + ", you lost " + str(
             bet) + ". Your total money is " + str(money - bet)
 
+def highest_card(plyr1_bet, plyr2_bet):
+    drawn_cards = []
+    plyr1_card = random.randint(1, 13)
+    plyr2_card = random.randint(1, 13)
+    while (drawn_cards.count(plyr1_card) != 1) and (drawn_cards.count(plyr2_card) != 1):
+        drawn_cards.append(plyr1_card)
+        drawn_cards.append(plyr2_card)
+
+    if(plyr1_card > plyr2_card):
+        return "Player1 won! with " + str(plyr1_card)
+    else:
+        return "Player2 won! with " + str(plyr2_card)
+
+def roulette(guess, bet):
+    number = random.randint(1,10)
+    if number == guess:
+        if number % 2 == 0:
+            return "You won! " + str(bet*2) + ". Total money is equal to " + str(money + bet)
+        else:
+            return "You won! " + str(bet) + ". Total money is equal to " + str(money + bet)
+    else:
+        return "Try again next time. You lost " + str(bet) + " pesos. " + "your reamaining money is " + str(money-bet) + " pesos"
 
 #Call your game of chance functions here
 print(coin_flip('head', 90))
 print(coin_flip('tail', 90))
 print(cho_han('even', 50))
+print(highest_card(50,20))
+print(roulette(1, 50))
