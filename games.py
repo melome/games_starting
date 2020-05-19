4dd48e82fcc0eb3da1b73707d3850ac636912e37
melome <shanleyrosero@gmail.com>
2020-05-19 17:49:56 +0800

    function for coin flip game

--- games.py
+++ games.py
@@ -3,10 +3,21 @@ import random
 money = 100
 
 #Write your game of chance functions here
-
-
-
-
+def coin_flip(guess, bet):
+   coin = random.randint(1, 2)
+   if coin == guess:
+     if coin == 1:
+       coin = "head"
+     else:
+       coin = "tail"
+     return "Congrats! the coin lands on " + coin + " you won " + str(bet) + " pesos. You now have a total money of " + str(money + bet)
+   else:
+     if coin == 1:
+       coin = "head"
+     else:
+       coin = "tail"
+       return "Better luck next time.. the coin lands on " + coin + " you lost " + str(bet) + ". Your total money is " + str(money - bet)
 
 
 #Call your game of chance functions here
+print(coin_flip(1, 30))
